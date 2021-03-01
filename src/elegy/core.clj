014e4327a71cbd3a33c9ff611dc26f50e5121fe7
Ryan Echols Knauber <ryanknauber@yahoo.com>
2021-02-28 20:40:50 -0800

    Documentation, more tweaks, and beginning of "look".

--- src/elegy/core.clj
+++ src/elegy/core.clj
@@ -2,36 +2,50 @@
   (:gen-class))
 
 ;; The type returned by this is "clojure.lang.Repeat"?
-(defn string-repeat [chosen-string reps]
+(defn string-repeat
+  "Repeats a given string a specified number of times."
+  [chosen-string reps]
   (repeat reps chosen-string))
 
-;; Print a collection
+;; Print a collection, each item on a new line.
 (defn print-coll
+  "Prints a given collection item-by-item on new lines."
   [coll]
   (for [x coll]
-    (print x))
+    (print x "\n"))
   )
 
-(def loogout "Listen up World, here it comes. Loogout ya bish. \n")
+;; A really cool string.
+(def loogout "Listen up World, here it comes. Loogout ya bish.")
 
+;; Print the really cool string many times.
 (print-coll (string-repeat loogout 10))
 
-
+;; A test of a for loop on concatenated string vectors.
 (for [x (concat ["Whales."] ["\n"])]
               (print x))
 
-(defn forever [a b c & d]
-  (if (nil? d)
-    (* a b c)
-    (loop [x d
-           y (* a b c)]
+;; Handle an infinite number of arguments, in this case multiplying them all.
+(defn do-this-to-everything
+  "Do `this` (a func), to `everything` (any number of args)."
+  [this & everything]
+  (if (nil? everything)
+    nil
+    (loop [x (rest everything)
+           y (first everything)
+           a this]
       (if (empty? x)
         y
-        (recur (rest x) (* y (first x))))
+        (recur (rest x) (this y (first x)) this))
       )
     ))
 
-(forever 10 2 3 4 10 10)
+(do-this-to-everything * 10 2 3 4 10 10)
+
+;; (defn look
+;;   ""
+;;   [x]
+;;   (do-this-to-everything + x))
 
 ;; (defn -main
 ;;   "I don't do a whole lot ... yet."
